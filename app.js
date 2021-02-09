d8e558ff8091c127429f284976a2f9f41b603692
Meghkonna Rumi <avimistry4800@gmail.com>
2021-02-09 19:05:07 +0600

    show lyrics

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const searchSongs = ()=>{
 
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
-
+    songContainer.innerHTML='';
     songs.forEach(song => {
         console.log(song);
         const songDiv = document.createElement('div');
@@ -18,11 +18,27 @@ const displaySongs = songs => {
                     <div class="col-md-9">
                         <h3 class="lyrics-name">${song.title}</h3>
                         <p class="author lead">Album by <span>${song.artist.name}</span></p>
-                    </div>
+                        <audio controls>
+                            <source src="${song.preview}" type="audio/mpeg">
+                        </audio>
+                        </div>
                     <div class="col-md-3 text-md-right text-center">
-                        <button class="btn btn-success">Get Lyrics</button>
+                        <button onclick= "getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
                     </div>
                     `;
         songContainer.appendChild(songDiv);
     })
+}
+
+const getLyric = (artist,title) =>{
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+
+}
+
+const displayLyrics = lyrics =>{
+    const lyricsDiv = document.getElementById("song-Lyrics");
+    lyricsDiv.innerText = lyrics;
 }
